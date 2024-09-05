2a4e343bbd05bbb0e0ead0eff8663e644dcd6b3d
Vishal Yadav <vishalyxo@gmail.com>
2024-09-05 13:28:02 +0530

    "Updated button styles in RTEField component"

--- app/admin/posts/form/components/RTEField.jsx
+++ app/admin/posts/form/components/RTEField.jsx
@@ -79,13 +79,13 @@ export function RTEField() {
     <div className="dark:text-white">
       <div className="mb-4">
         <button 
-          className={`px-4 py-2 mr-2 rounded-md ${activeTab === 'editor' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+          className={` text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ${activeTab === 'editor' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
           onClick={() => setActiveTab('editor')}
         >
           Editor
         </button>
         <button 
-          className={`px-4 py-2 rounded-md ${activeTab === 'preview' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+          className={`text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ${activeTab === 'preview' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
           onClick={() => setActiveTab('preview')}
         >
           Preview
